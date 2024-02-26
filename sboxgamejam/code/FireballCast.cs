a3a3e031992e0a75fec0ca64350c9be5263857ea
DARKSAYONE <skyhard2017@yandex.ru>
2024-02-26 17:35:36 +0300

    СИНХРОНИЗАЦИЯ
    Ознакомившись с вот этим https://docs.facepunch.com/s/sbox-dev/doc/networking-multiplayer-kaVboe3yRD#h-destroy-networked-gameobject
    
    Я сделал так что объект fireball который спавнится игроком является СЕТЕВЫМ, от чего его видят все игроки на сервере. По сути это то что нам было нужно.
    
    Теперь чтобы окончательно завершить сие тестовую работу нужно.
    1.Воспользоваться таймером и удалять фаерболл через 5 секунд.
    2.Воспользоваться методом который считывает соприкосновение коллайдеров чтобы удалять фаерболл при соприкосновении со стенкой.
    3.Научится за экземпляр игрока обращаться к объекту на сцене.

--- sboxgamejam/code/FireballCast.cs
+++ sboxgamejam/code/FireballCast.cs
@@ -5,20 +5,32 @@ public sealed class FireballCast : Component
 
 	[Property] public GameObject fireballPrefab;
 	[Property] public GameObject fireballspawnPos;
+	[Property] public GameObject cameraFind;
+	[Property] private bool findCamera = false; // Prosti ya shas pishy polnyi govnocod
 
 	protected override void OnAwake()
 	{
 		base.OnAwake();
+
+		//fireballspawnPos = GameObject.Scene.Directory.FindByName( "Camera" );
 		
+
 	}
 	protected override void OnUpdate()
 	{
+		
+
 		if ( !IsProxy )
 		{
+			
+			
+
 
 			if ( Input.Pressed( "Attack1" ) )
 			{
-				GameObject fireballAttack = fireballPrefab.Clone( fireballspawnPos.Transform.Position );
+				var netFireball = fireballPrefab.Clone( fireballspawnPos.Transform.Position+Vector3.Forward );
+				netFireball.NetworkSpawn();
+
 			}
 		}
 		else
