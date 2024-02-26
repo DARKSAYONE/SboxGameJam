b20b34c78b99825f4e7c9798de0ddfd0e5a84ca1
That_Abyss_Egg <probablynotfaputa@gmail.com>
2024-02-27 01:35:08 +0300

    Fixed code style, added the mechanic to delete the fireballs after some time after its creation & fixed the fireballs rotation issue.

--- sboxgamejam/code/Fireball.cs
+++ sboxgamejam/code/Fireball.cs
@@ -4,31 +4,24 @@ public sealed class Fireball : Component
 {
 	[Property] private Rigidbody rb;
 	[Property] public float speed = 100;
-	
 
-
-
-
+	private float creationTime;
 	protected override void OnStart()
 	{
 		base.OnStart();
-		
-		
+		creationTime = Time.Now;
 		rb = Components.Get<Rigidbody>();
 	}
 
-
-
-	
-
 	protected override void OnUpdate()
 	{
+		float timeSinceCreation = Time.Now - creationTime;
 
+		if (timeSinceCreation > 5f)
+		{
+			GameObject.Destroy();
+		}
 		//rb.ApplyForce( Vector3.Forward * speed*Time.Delta ); попытка использовать Rigidbody
-
-		Transform.Position += Vector3.Forward * speed * Time.Delta;
-		
-		
+		Transform.Position += Transform.Rotation.Forward * speed * Time.Delta;
 	}
 }
-
--- sboxgamejam/code/FireballCast.cs
+++ sboxgamejam/code/FireballCast.cs
@@ -1,41 +1,36 @@
 using Sandbox;
+using System.Diagnostics;
 
 public sealed class FireballCast : Component
 {
+    [Property]
+    public GameObject FireballPrefab;
+    [Property]
+    public GameObject FireballSpawnPos;
+    [Property]
+    public GameObject CameraFind;
 
-	[Property] public GameObject fireballPrefab;
-	[Property] public GameObject fireballspawnPos;
-	[Property] public GameObject cameraFind;
-	[Property] private bool findCamera = false; // Prosti ya shas pishy polnyi govnocod
+    private bool findCamera;
 
-	protected override void OnAwake()
-	{
-		base.OnAwake();
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+        // fireballspawnPos = GameObject.Scene.Directory.FindByName("Camera");
+    }
 
-		//fireballspawnPos = GameObject.Scene.Directory.FindByName( "Camera" );
-		
-
-	}
-	protected override void OnUpdate()
-	{
-		
-
-		if ( !IsProxy )
-		{
-			
-			
-
-
-			if ( Input.Pressed( "Attack1" ) )
-			{
-				var netFireball = fireballPrefab.Clone( fireballspawnPos.Transform.Position+Vector3.Forward );
-				netFireball.NetworkSpawn();
-
-			}
-		}
-		else
-		{
-			Log.Info( "IsProxy ne srabotal" );
-		}
-	}
+    protected override void OnUpdate()
+    {
+        if (!IsProxy)
+        {
+            if (Input.Pressed("Attack1"))
+            {
+                var netFireball = FireballPrefab.Clone(FireballSpawnPos.Transform.Position + Vector3.Forward, FireballSpawnPos.Transform.Rotation);
+                netFireball.NetworkSpawn();
+            }
+        }
+        else
+        {
+            Log.Info("IsProxy didn't work.");
+        }
+    }
 }
