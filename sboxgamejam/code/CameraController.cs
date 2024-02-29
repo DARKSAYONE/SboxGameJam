05e8b448ec8e84819a5883aefd4de48e14283444
That_Abyss_Egg <probablynotfaputa@gmail.com>
2024-02-29 23:55:59 +0300

    More attempts at trying to fix the multiple camera problem

---
+++ sboxgamejam/code/CameraController.cs
@@ -0,0 +1,16 @@
+using Sandbox;
+
+public sealed class CameraController : Component
+{
+	protected override void OnStart()
+	{
+		base.OnStart();
+		Scene.Camera.Enabled = false;
+		GameObject.Network.SetOwnerTransfer( OwnerTransfer.Takeover );
+		Log.Info( $"Owner is {Network.OwnerId}" );
+	}
+	protected override void OnUpdate()
+	{
+
+	}
+}
