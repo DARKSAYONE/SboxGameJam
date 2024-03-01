659004cfb94870777704a9fafa7c9e1639363a23
That_Abyss_Egg <probablynotfaputa@gmail.com>
2024-03-01 23:59:18 +0300

    Added getters & setters for player stats & made them synced between the players

--- sboxgamejam/code/PlayerStats.cs
+++ sboxgamejam/code/PlayerStats.cs
@@ -2,15 +2,15 @@ using Sandbox;
 
 public sealed class PlayerStats : Component
 {
-	[Property] public int Level;
-	[Property] public int Experience;
-	[Property] public int HP;
-	[Property] public int Mana;
-	[Property] public float ManaRegen;
-	[Property] public int PhysicalPower;
-	[Property] public int MindPower;
-	[Property] public int Protection;
-	[Property] public int Fortitude;
-	[Property] public float MovementSpeed;
-	[Property] public float HitSpeed;
+	[Sync] public int Level { get; set; }
+	[Sync] public int Experience { get; set; }
+	[Sync] public int HP { get; set; }
+	[Sync] public int Mana { get; set; }
+	[Sync] public float ManaRegen { get; set; }
+	[Sync] public int PhysicalPower { get; set; }
+	[Sync] public int MindPower { get; set; }
+	[Sync] public int Protection { get; set; }
+	[Sync] public int Fortitude { get; set; }
+	[Sync] public float MovementSpeed { get; set; }
+	[Sync] public float HitSpeed { get; set; }
 }
