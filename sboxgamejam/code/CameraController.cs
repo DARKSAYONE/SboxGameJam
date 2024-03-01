3cbf7bf5f67911f9876d31b3c4cf20a6f524cacb
DARKSAYONE <skyhard2017@yandex.ru>
2024-03-01 18:01:42 +0300

    Try to camerafix.

--- sboxgamejam/code/CameraController.cs
+++ sboxgamejam/code/CameraController.cs
@@ -5,9 +5,12 @@ public sealed class CameraController : Component
 	protected override void OnStart()
 	{
 		base.OnStart();
-		Scene.Camera.Enabled = false;
-		GameObject.Network.SetOwnerTransfer( OwnerTransfer.Takeover );
-		Log.Info( $"Owner is {Network.OwnerId}" );
+		if ( !IsProxy )
+		{
+			Scene.Camera.Enabled = false;
+			GameObject.Network.SetOwnerTransfer( OwnerTransfer.Takeover );
+			Log.Info( $"Owner is {Network.OwnerId}" );
+		}
 	}
 	protected override void OnUpdate()
 	{
