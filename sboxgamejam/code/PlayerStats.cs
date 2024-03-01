beaa81a44d057e12ca601cfecfeafcad0b28d328
That_Abyss_Egg <probablynotfaputa@gmail.com>
2024-03-01 23:55:07 +0300

    Added player stats

--- sboxgamejam/code/PlayerStats.cs
+++ sboxgamejam/code/PlayerStats.cs
@@ -2,11 +2,15 @@ using Sandbox;
 
 public sealed class PlayerStats : Component
 {
-	//[Property]
-	//public int 
-
-	protected override void OnUpdate()
-	{
-
-	}
+	[Property] public int Level;
+	[Property] public int Experience;
+	[Property] public int HP;
+	[Property] public int Mana;
+	[Property] public float ManaRegen;
+	[Property] public int PhysicalPower;
+	[Property] public int MindPower;
+	[Property] public int Protection;
+	[Property] public int Fortitude;
+	[Property] public float MovementSpeed;
+	[Property] public float HitSpeed;
 }
