3d39a5eae56a9115a50ba672267d3f3557e9042e
DARKSAYONE <skyhard2017@yandex.ru>
2024-03-08 20:25:08 +0300

    GameManager script UPD.
    Доработан, но не завершен скрипт GameManager.cs.
    
    Работает тот по принципу.
    1.Ожидает появление двух игроков на сцене.
    2.Запускает 10-и секундный отсчет по истечению которого начинается PVP Round
    3. На данный момент PVP Round лишь переносит игроков на свои стартовые зоны.
    
    Что нужно доработать: В игре должна присутствовать система смерти чтобы игровой мененджер мог её отследить.

--- sboxgamejam/code/GameManager.cs
+++ sboxgamejam/code/GameManager.cs
@@ -8,13 +8,14 @@ public sealed class GameManager : Component
 	[Property] private List<GameObject> Players = new List<GameObject>();
 	[Property] private List<GameObject> SpawnPositions = new List<GameObject>();
 	[Property] private List<int> PlayersScore = new List<int>() { 0, 0 };
+	[Property] private GameObject BaseSpawnPos;
 	
 	private bool AllPlayersReady = false;
-	private bool PVPRoundActive = false;
 
-	private float timer = 10f;
+	private bool RoundOn = false;
+	private float RoundStartTimer = 10f;
 	private bool timerGo = false;
-
+	private bool DeployPlayers = false;
 	protected override void OnStart()
 	{
 		base.OnStart();
@@ -26,13 +27,18 @@ public sealed class GameManager : Component
 		if(!AllPlayersReady)
 		PlayerInitialization();
 
-		if(AllPlayersReady && !PVPRoundActive)
-		{	
-			PVPRound();
-			timerGo = true;
+		if(AllPlayersReady && !RoundOn)
+		{
+			TimerBeforeRoundStart();
 		}
 
-		
+		if(AllPlayersReady && RoundOn)
+		{
+			PVPRound();
+			
+		}
+
+		//RoundStopped();
 	}
 
 	void PlayerInitialization()
@@ -43,30 +49,73 @@ public sealed class GameManager : Component
 		}
 		else
 		{
-			
 			AllPlayersReady = true;
 			Log.Info ( "All players connect" );
 		}
 		Log.Info( Players.Count );		
 	}
-
 	
 	void PVPRound()
 	{
+		bool CheckWin = false;
+		if (!DeployPlayers)
+		{
+			for ( int i = 0; i < Players.Count; i++ )
+			{
+				Players[i].Transform.Position = SpawnPositions[i].Transform.Position;
+			}
+			Log.Info( "Players tele to spawn positions" );
+			DeployPlayers = true;
+			CheckWin = true;
+		}
+
+		if( CheckWin )
+		{
+			//Написать шнягу что будет ждать информацию о смерти одного из игроков.
+		}
 		
 	}
 
-	void TimerStart()
+	void TimerBeforeRoundStart()
 	{
-		if ( timer > 0 )
+		if ( RoundStartTimer > 0 )
 		{
-			timer -= Time.Delta;
-			Log.Info( timer );
+			RoundStartTimer -= Time.Delta;
+			Log.Info( RoundStartTimer );
 		}
-		else if(timer < 0)
+		else if(RoundStartTimer < 0)
 		{
-			timerGo = false;
-			timer = 10f;
+
+			RoundOn = true;
+			RoundStartTimer = 10f;
 		}
 	}
+
+
+
+
+
+	/*[Broadcast]
+	void RoundStopped()
+	{
+		if(Input.Pressed("use"))
+		{
+			for ( int i = 0; i < Players.Count; i++ )
+				Players[i].Transform.Position = BaseSpawnPos.Transform.Position;
+
+			RoundOn = false;
+		}
+
+	}
+	*/
+
+	/*void DeployPlayersToArena()
+	{
+		for ( int i = 0; i < Players.Count; i++ )
+		{
+			Players[i].Transform.Position = SpawnPositions[i].Transform.Position;
+		}
+		Log.Info( "Players tele to spawn positions" );
+	}
+	*/
 }
