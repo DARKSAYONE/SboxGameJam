2e9ce5ecb6ed79b05e41d7b6f521b53159aad58a
DARKSAYONE <skyhard2017@yandex.ru>
2024-03-14 19:25:01 +0300

    Add IceArrow

---
+++ sboxgamejam/code/IceArrow.cs
@@ -0,0 +1,53 @@
+using Sandbox;
+
+public sealed class IceArrow : Component, Component.ICollisionListener
+{
+
+	[Property] public float speed = 300;
+
+	private Rigidbody rb;
+	private float creationTime;
+
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+		creationTime = Time.Now;
+		rb = Components.Get<Rigidbody>();
+	}
+
+
+	protected override void OnUpdate()
+	{
+		float timeSinceCreation = Time.Now - creationTime;
+
+		if ( timeSinceCreation > 5f )
+		{
+			GameObject.Destroy();
+		}
+		rb.Velocity = Transform.Rotation.Forward * speed;
+	}
+
+
+
+	public void OnCollisionStart( Collision other )
+	{
+		if ( IsProxy )
+			return;
+
+		if ( other.Other.GameObject.Tags.Has( "player" ) )
+		{
+			var ownerName = GameObject.Name.Substring( 0, GameObject.Name.LastIndexOf( " - " ) );
+			Log.Info( ownerName );
+
+			var attackerGUID = Scene.GetAllObjects( true ).FirstOrDefault( x => x.Name == ownerName ).Id;
+			other.Other.GameObject.Parent.Components.Get<PlayerController>().TakeDamage( attackerGUID );
+		}
+		else
+		{
+			Log.Info( "IceArrow hit the obj" );
+		}
+
+		GameObject.Destroy();
+	}
+}
